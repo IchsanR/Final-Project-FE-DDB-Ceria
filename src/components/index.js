a2e2050a5dbd31f208dbefd63e7bf93eda0e3a11
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2023-06-15 10:22:58 +0700

    add inputs component to login page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Buttons from "./atom/Buttons";
+import Inputs from "./molecule/Inputs";
 import Logo from "./atom/Logo";
 
-export { Buttons, Logo };
+export { Buttons, Logo, Inputs };
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,28 @@
-import React, { Fragment } from "react";
-import { Logo } from "../../components";
+import React from "react";
+import { Inputs, Logo } from "../../components";
 
 const Login = () => {
 	return (
-		<Fragment>
-			<div className="flex justify-center items-center w-screen h-screen">
+		<div className="flex justify-center items-center flex-col h-screen w-screen px-4">
+			<header>
 				<Logo />
-			</div>
-		</Fragment>
+			</header>
+			<main className="md:w-[500px] w-full">
+				<form className=" w-full">
+					<div className="my-10">
+						<h1 className="font-bold text-2xl">Sign in to your account</h1>
+					</div>
+					<div className="mb-3">
+						<Inputs id={"email"} placeholder={"name@company.com"} label={"Your Email"} type={"email"}
+						/>
+					</div>
+					<div className="mb-3">
+						<Inputs id={"password"} placeholder={"**********"} label={"Your Password"} type={"password"}
+						/>
+					</div>
+				</form>
+			</main>
+		</div>
 	);
 };
 
