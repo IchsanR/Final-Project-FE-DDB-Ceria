b2ed5917fc82d2e6d03c8093aa9d6ee2b1693fee
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2023-06-15 10:41:59 +0700

    finish style on login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Inputs, Logo } from "../../components";
+import { Buttons, Inputs, Logo } from "../../components";
+import { Link } from "react-router-dom";
 
 const Login = () => {
 	return (
@@ -20,7 +21,22 @@ const Login = () => {
 						<Inputs id={"password"} placeholder={"**********"} label={"Your Password"} type={"password"}
 						/>
 					</div>
+					<div className="flex justify-between my-3">
+						<div>
+							<input type="checkbox" id="rememberMe" className="w-4 h-4 mr-3 top-1/2 relative -translate-y-1/2" />
+							<label htmlFor="rememberMe" className="text-[#6B7280]">Remember Me</label>
+						</div>
+						<div>
+							<Link to={'/'} className="text-violet-800 font-semibold" >Forgot Password?</Link>
+						</div>
+					</div>
+					<div className="my-6">
+						<Buttons type={'submit'} classname={'w-full bg-violet-800 text-white h-12 rounded-lg hover:bg-violet-900'} description={'Sign In'} />
+					</div>
 				</form>
+				<div>
+					<p className="text-[#6B7280]" >Don't have account yet? <span><Link className="text-violet-800 font-semibold">Sign Up</Link></span></p>
+				</div>
 			</main>
 		</div>
 	);
