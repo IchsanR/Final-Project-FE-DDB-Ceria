50ac5ae5bf9987be8cd286856b80c6dbfc5c358b
unknown <dodorobe564@gmail.com>
2023-06-21 10:40:32 +0700

    Fix Register Form

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -143,7 +143,7 @@ const Register = () => {
               id={"phone"}
               placeholder={"0812xxxxxx"}
               label={"Phone Number"}
-              type={"tel"}
+              type={"number"}
               name={"phone"}
               onChange={(e) => setForm({ ...form, phone: e.target.value })}
             />
@@ -172,7 +172,7 @@ const Register = () => {
                     viewBox="0 0 16 16"
                     fill="currentColor"
                   >
-                    <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/> <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/> <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
+                    <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" /> <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" /> <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
                   </svg>
                 ) : (
                   <svg
@@ -200,32 +200,30 @@ const Register = () => {
                   </div>
                   <div className="text-right">
                     <span
-                      className={`text-sm font-semibold inline-block ${
-                        passwordStrength.score >= 3
+                      className={`text-sm font-semibold inline-block ${passwordStrength.score >= 3
                           ? "text-green-500"
                           : passwordStrength.score === 2
-                          ? "text-orange-500"
-                          : "text-red-500"
-                      }`}
+                            ? "text-orange-500"
+                            : "text-red-500"
+                        }`}
                     >
                       {passwordStrength.score >= 3
                         ? "Kuat"
                         : passwordStrength.score === 2
-                        ? "Sedang"
-                        : "Lemah"}
+                          ? "Sedang"
+                          : "Lemah"}
                     </span>
                   </div>
                 </div>
                 <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-200">
                   <div
                     style={{ width: `${(passwordStrength.score + 1) * 20}%` }}
-                    className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center ${
-                      passwordStrength.score >= 3
+                    className={`shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center ${passwordStrength.score >= 3
                         ? "bg-green-500"
                         : passwordStrength.score === 2
-                        ? "bg-orange-500"
-                        : "bg-red-500"
-                    }`}
+                          ? "bg-orange-500"
+                          : "bg-red-500"
+                      }`}
                   ></div>
                 </div>
                 <div className="text-sm text-gray-500">
@@ -270,7 +268,7 @@ const Register = () => {
           <p className="text-[#6B7280]">
             Already have an account?{" "}
             <span>
-              <Link className="text-violet-800 font-semibold">Login Here</Link>
+              <Link to="/login" className="text-violet-800 font-semibold">Login Here</Link>
             </span>
           </p>
         </div>
