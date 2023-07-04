4f22ae30090e2ace7ea1cf690fad09c3f8b0f377
iqbalkurnia <iqbalkurnia091@gmail.com>
2023-07-04 16:23:46 +0700

    success solve bug reactCsv

--- src/components/molecule/ExportCsv.jsx
+++ src/components/molecule/ExportCsv.jsx
@@ -1,11 +1,28 @@
-// yang lama
-// -------------------------------------- 
 // import React, { useEffect, useState } from "react";
 // import { Button, Modal } from "flowbite-react";
 // import Datepicker from "react-tailwindcss-datepicker";
 // import { CSVLink } from "react-csv";
 // import Swal from "sweetalert2";
 // import { apiExportCsv } from "../../redux/api/exportCsvApi";
+
+
+// const Exports = ({ data, filename, loading, onClick }) => {
+//     return (
+//       <>
+//         {loading === true ? (
+//           <button disabled className='border rounded-lg'>
+//             Export
+//           </button>
+//         ) : (
+//           <CSVLink data={data} filename={filename}>
+//             <button onClick={onClick} className='border rounded-lg'>
+//               Export
+//             </button>
+//           </CSVLink>
+//         )}
+//       </>
+//     );
+//   };
 
 // const ExportCsv = () => {
 //   const [openModal, setOpenModal] = useState(false);
@@ -16,7 +33,7 @@
 //   });
 //   const [status, setStatus] = useState("");
 //   const [latestDate, setLatestDate] = useState('');
-//   const [loading, setLoading] = useState(false);
+//   const [loading, setLoading] = useState(true);
 
 //   // date picker
 //   const handleValueChange = (newValue) => {
@@ -44,7 +61,7 @@
 //             timer: 3000,
 //           });
 //         }
-//         setLoading(false);
+//         // setLoading(false);
 //         return false;
 //       }
 //     }
@@ -126,15 +143,30 @@
 
 // export default ExportCsv;
 
-
-// yang baru
-// --------------------------------------
 import React, { useEffect, useState } from "react";
 import { Button, Modal } from "flowbite-react";
 import Datepicker from "react-tailwindcss-datepicker";
 import { CSVLink } from "react-csv";
 import Swal from "sweetalert2";
 import { apiExportCsv } from "../../redux/api/exportCsvApi";
+
+export const Exports = ({ data, filename, loading, onClick }) => {
+  return (
+    <>
+      {loading === true ? (
+        <button disabled className='border rounded-lg'>
+          Export
+        </button>
+      ) : (
+        <CSVLink data={data} filename={filename}>
+          <button onClick={onClick} className='border rounded-lg'>
+            Export
+          </button>
+        </CSVLink>
+      )}
+    </>
+  );
+};
 
 const ExportCsv = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -144,61 +176,64 @@ const ExportCsv = () => {
     endDate: "",
   });
   const [status, setStatus] = useState("");
-  const [latestDate, setLatestDate] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [latestDate, setLatestDate] = useState("");
+  const [loading, setLoading] = useState(true);
 
   // date picker
   const handleValueChange = (newValue) => {
+    // console.log("newValue:", newValue);
     setDate(newValue);
+    setLoading(true)
   };
 
   // get token
   const token = localStorage.getItem("token") || sessionStorage.getItem("token");
 
   // export data
-  const getData = async () => {
-    if (!loading) {
-      setLoading(true);
-      try {
-        const response = await apiExportCsv(token, status, date.startDate, date.endDate);
-        setDataCsv(response.data);
+  const getData = () => {
+    // if(!loading) {
+    // setLoading(true);
+    apiExportCsv(token, status, date.startDate, date.endDate)
+      .then((res) => {
+        setDataCsv(res.data);
         setLoading(false);
-      } catch (err) {
-        Swal.fire({
-          title: "Oops..",
-          text: "Data tidak tersedia",
-          icon: "error",
-          timer: 3000,
-        });
-        setLoading(false);
-        setError(err);
-      }
-    }
-  };
-
-  // get current date export
-  const handleDownload = () => {
-    const currentDate = new Date().toLocaleDateString('id-ID');
-    setLatestDate(currentDate);
-  };
-
-  // handle export
-  const handleExport = () => {
-    setOpenModal(false);
-    setDate({ ...date, startDate: "", endDate: "" });
-    handleDownload();
-  };
-
-  // handle close
-  const handleClose = () => {
-    setOpenModal(false);
-    setDate({ ...date, startDate: "", endDate: "" });
+      })
+      .catch((err) => {
+        if (err) {
+          Swal.fire({
+            title: "Oops..",
+            text: `data tidak tersedia`,
+            icon: "error",
+            timer: 3000,
+          });
+        }
+      });
+    // }
   };
 
   useEffect(() => {
     getData();
   }, [date, status]);
+
+  // get current date export
+  const handleDownload = () => {
+    const currentDate = new Date().toLocaleDateString("id-ID");
+    setLatestDate(currentDate);
+  };
+
+  // handle export
+  const handleExport = () => {
+    return (setOpenModal(false), setDate({ ...date, startDate: "", endDate: "" }), handleDownload());
+  };
+
+  // handle close
+  const handleClose = () => {
+    return (setOpenModal(false), setDate({ ...date, startDate: "", endDate: "" }));
+  };
+
+  useEffect(() => {
+    handleExport();
+  }, []);
 
   return (
     <>
@@ -214,11 +249,10 @@ const ExportCsv = () => {
               <select
                 id='status'
                 className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-5'
-                onChange={(e) => setStatus(e.target.value)}
-              >
+                onChange={(e) => setStatus(e.target.value)}>
                 <option value=''>All Export</option>
                 <option value='SUCCESS'>Export status success</option>
-                <option value='WAITING_FOR_DEBITTED'>Export status waiting</option>
+                <option value='WAITING_FOR_DEBITTED'>Export status waitting</option>
               </select>
               <div>
                 <label className='block text-sm font-medium text-gray-900 dark:text-white pb-2'>Choose Date</label>
@@ -227,13 +261,18 @@ const ExportCsv = () => {
             </div>
           </div>
           <Modal.Footer>
-            <CSVLink
+            {/* <Button
+              onClick={handleExport}>
+              <CSVLink data={dataCsv} filename={`data_transaction_${latestDate}.csv`}>
+                Export
+              </CSVLink>
+            </Button> */}
+            <Exports
               data={dataCsv}
               filename={`data_transaction_${latestDate}.csv`}
+              loading={loading}
               onClick={handleExport}
-            >
-              {loading ? "Loading csv..." : "Download me"}
-            </CSVLink>
+            />
             <Button color='gray' onClick={handleClose}>
               Cancel
             </Button>
