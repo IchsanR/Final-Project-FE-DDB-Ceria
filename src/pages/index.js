7330c17cbc79cdcfd07e6bfbc8807834262794a7
unknown <dodorobe564@gmail.com>
2023-07-06 14:17:20 +0700

    Separate Verificationpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import ForgetPassword from "./ForgetPassword";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
-import Verificationpage from "./Register/Verificationpage";
+import Verificationpage from "./Verificationpage";
 export { Home, Login, Register, Verificationpage, ForgetPassword };
