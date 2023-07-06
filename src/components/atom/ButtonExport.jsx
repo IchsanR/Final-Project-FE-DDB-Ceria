0cb1aa350c980b96335762556b6b33e18654881c
iqbalkurnia <iqbalkurnia091@gmail.com>
2023-07-06 10:44:41 +0700

    add loading spinner in export csv

--- src/components/atom/ButtonExport.jsx
+++ src/components/atom/ButtonExport.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CSVLink } from "react-csv";
 import { Button } from "flowbite-react";
+import Spinner from "./Spinner";
 
 const Exports = (props) => {
   const {data, filename, loading, onClick} = props
+  const [showSpinner, setShowSpinner] = useState(false);
+
+  useEffect(() => {
+    if (loading) {
+      setShowSpinner(true);
+      const timer = setTimeout(() => {
+        setShowSpinner(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [loading]);
+
   return (
     <>
       {loading === true ? (
         <Button disabled className='border rounded-lg'>
-          Export
+          {showSpinner ? <Spinner />  : "export"}
         </Button>
       ) : (
         <CSVLink data={data} filename={filename}>
           <Button onClick={onClick} className='border rounded-lg'>
-            Export
+          {showSpinner ? "export" : <Spinner /> ? "export" : "exports"}
           </Button>
         </CSVLink>
       )}
