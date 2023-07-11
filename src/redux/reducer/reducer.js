7214d593e65e6c0656e85d10b3914f8ed2ac1706
iqbalkurnia <iqbalkurnia091@gmail.com>
2023-07-11 10:22:40 +0700

    solve conflict file reducer

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,14 +1,4 @@
 import { combineReducers } from "redux";
-<<<<<<< HEAD
-import { userApi } from "../api/User";
-// import { apiModal } from "../api/exportCsvApi";
-// import exportsCsv from "./export";
-
-const rootReducer = combineReducers({
-  [userApi.reducerPath]: userApi.reducer,
-  // [apiModal.reducerPath]: apiModal.reducer,
-  // apiModal : exportsCsv
-=======
 import loginReducer from "./login";
 import registerReducer from "./register";
 import sendVerificationReducer from "./sendVerification";
@@ -19,7 +9,6 @@ const rootReducer = combineReducers({
   register: registerReducer,
   sendVerification: sendVerificationReducer,
   compareVerification: compareVerificationReducer
->>>>>>> 087278e2e4b477399f09685007b1831623e96311
 });
 
 export default rootReducer;
